67f4ac027515fff08f77246e5c467c1855335fc9
Randria Jimmy <randriajimmy@gmail.com>
2022-05-20 02:04:55 +0200

    new test push to vercel

---
+++ index.js
@@ -0,0 +1,18 @@
+// Add Express
+const express = require("express");
+
+// Initialize Express
+const app = express();
+
+// Create GET request
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
+});
+
+// Initialize server
+app.listen(5000, () => {
+  console.log("Running on port 5000.");
+});
+
+// Export the Express API
+module.exports = app;
